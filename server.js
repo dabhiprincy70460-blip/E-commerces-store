46d506fea72405292e43a30bd9dc1fed0e9e4a19
Dabhi Princy <dabhiprincy70460@gmail.com>
2026-02-02 19:02:04 +0530

    Initialize Express server with MongoDB connection

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
